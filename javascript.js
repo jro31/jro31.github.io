5e88a4addf8b1f21625b30eea5c8c54f82bafe8b
Jethro Williams <jethrowilliams4@hotmail.com>
2020-08-25 11:55:07 +0100

    Add jquery, bootstrap and FontAwesome and add some basic styling

--- javascript.js
+++ javascript.js
@@ -1,36 +1,38 @@
-var shortPause = 500
-var longPause = 3000
-
 function typeWriter() {
+  var shortPause = 500
+  var longPause = 3000
+  var veryLongPause = 6000
   var initialPause = 2000
   setTimeout( function(){
     var phrases = [
-      {text: 'A duck walks into a bar and asks the bartender', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
+      {text: 'A duck walks into a bar.', pauseAfter: longPause, typewriter: 'narrator', clearAfter: true},
+      {text: 'He asks the bartender,', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
       {text: '"Got any bread?"', pauseAfter: longPause, typewriter: 'duck', clearAfter: true},
-      {text: 'The bartender says', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
-      {text: '"No"', pauseAfter: longPause, typewriter: 'bartender', clearAfter: true},
-      {text: 'The duck says again', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
+      {text: 'The bartender says,', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
+      {text: '"No."', pauseAfter: longPause, typewriter: 'bartender', clearAfter: true},
+      {text: 'The duck says again,', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
       {text: '"Got any bread?"', pauseAfter: longPause, typewriter: 'duck', clearAfter: true},
-      {text: 'The bartender says', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
-      {text: '"No"', pauseAfter: longPause, typewriter: 'bartender', clearAfter: true},
-      {text: 'Again, the duck asks', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
+      {text: 'The bartender says,', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
+      {text: '"No."', pauseAfter: longPause, typewriter: 'bartender', clearAfter: true},
+      {text: 'Again, the duck asks,', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
       {text: '"Got any bread?"', pauseAfter: longPause, typewriter: 'duck', clearAfter: true},
-      {text: 'The bartender says', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
-      {text: '"No, we have no bread"', pauseAfter: longPause, typewriter: 'bartender', clearAfter: true},
-      {text: 'The duck repeats', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
+      {text: 'The bartender says,', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
+      {text: '"No, we have no bread."', pauseAfter: longPause, typewriter: 'bartender', clearAfter: true},
+      {text: 'The duck repeats,', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
       {text: '"Got any bread?"', pauseAfter: longPause, typewriter: 'duck', clearAfter: true},
-      {text: 'The bartender angrily says', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
-      {text: '"No, we haven\'t got any bread"', pauseAfter: longPause, typewriter: 'bartender', clearAfter: true},
-      {text: 'The duck says', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
+      {text: 'The bartender angrily says,', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
+      {text: '"No, we haven\'t got any bread."', pauseAfter: longPause, typewriter: 'bartender', clearAfter: true},
+      {text: 'The duck says,', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
       {text: '"Got any bread?"', pauseAfter: longPause, typewriter: 'duck', clearAfter: true},
-      {text: 'The bartender says', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
+      {text: 'The bartender says,', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
       {text: '"No, are you deaf? We haven\'t got any bread, ask me again and I\'ll nail your beak to the bar, you irritating little bird!"', pauseAfter: longPause, typewriter: 'bartender', clearAfter: true},
-      {text: 'The duck says', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
+      {text: 'The duck says,', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
       {text: '"Got any nails?"', pauseAfter: longPause, typewriter: 'duck', clearAfter: true},
-      {text: 'The bartender says', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
-      {text: '"No"', pauseAfter: longPause, typewriter: 'bartender', clearAfter: true},
-      {text: 'The duck says', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
-      {text: '"Got any bread?"', pauseAfter: longPause, typewriter: 'duck', clearAfter: true}
+      {text: 'The bartender says,', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
+      {text: '"No."', pauseAfter: longPause, typewriter: 'bartender', clearAfter: true},
+      {text: 'The duck says,', pauseAfter: shortPause, typewriter: 'narrator', clearAfter: false},
+      {text: '"Got any bread?"', pauseAfter: veryLongPause, typewriter: 'duck', clearAfter: true},
+      {text: '"Quack quack"', pauseAfter: veryLongPause, typewriter: 'duck', clearAfter: true}
     ];
     var speed = 50;
     var totalPause = 0
@@ -49,5 +51,9 @@ function typeWriter() {
     })
   }, initialPause)
 }
+
+$('#hamburger').click(function() {
+  console.log('🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀')
+});
 
 window.onload = typeWriter;
