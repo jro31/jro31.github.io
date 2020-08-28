4db4ea278377c849db9d20af502f686271a017b8
Jethro Williams <jethrowilliams4@hotmail.com>
2020-08-28 15:40:23 +0100

    Dynamically add projects timeline - part 1

--- javascript.js
+++ javascript.js
@@ -36,6 +36,64 @@ function menuItems() {
   return menuItemsArray
 }
 
+function populateTimeline() {
+  addTimelineContents()
+  addTimelineGaps()
+}
+
+function addTimelineContents() {
+  $.each(timelineContents(), function(index, item) {
+    $('#timeline-container').append(
+      '<div class="row">' +
+        '<div class="col-12 col-md-10 offset-md-1">' +
+          '<div class="d-flex justify-content-center timeline-part">' +
+            '<div class="col-2 pl-2 py-2 pr-0 pl-md-5 py-md-3">' +
+              (item.year ? `<p class="timeline-year text-center">${item.year}</p>` : '') +
+            '</div>' +
+            (index === 0 ? '<div class="col-2 col-md-1 pt-2 px-0 pt-md-3 d-flex flex-column align-items-center">' : '<div class="col-2 col-md-1 px-0 d-flex flex-column align-items-center">' ) +
+              (index !== 0 ? '<div class="timeline-line timeline-top-line"></div>' : '') +
+              (item.year ? '<div class="timeline-circle"></div>' : '' ) +
+              '<div class="timeline-line timeline-bottom-line"></div>' +
+            '</div>' +
+            '<div class="col-8 col-md-9 pl-0 pl-md-2 pr-2 py-2 pr-md-5 py-md-3">' +
+              `<h2 class="text-left">${item.title}</h2>` +
+              `<h4>${item.languages.join(', ')}</h4>` +
+              '<p>' +
+                item.description +
+              '</p>' +
+              '<p>' +
+                `<a href="${item.github_url}" target="_blank">View on Github</a>` +
+              '</p>' +
+            '</div>' +
+          '</div>' +
+        '</div>' +
+      '</div>'
+    )
+  })
+}
+
+function timelineContents() {
+  // { year: , title: , languages: [], description: "", site_url: , github_url: },
+  return [
+    { year: 2018, title: 'Hangman', languages: ['Swift'], description: "My coding journey started in 2018 by self-teaching Swift using the 'Hacking with Swift' book, as my initial goal from coding was to make iPhone apps. The book issued various challenges, and this one I consider the first app that I really created myself (I don't count 'Hello World'), as it didn't offer any example code to get you started. So it was just me, the things I'd learnt and Google, against the world. Three days and lots of swearing later, I'd done it; a fully-working iPhone hangman app.", site_url: null, github_url: 'https://github.com/jro31/Hangman' },
+    { year: 2018, title: 'Notes', languages: ['Swift'], description: "Imitation is the highest form of flattery, and four challenges later, we'd gone from novice coders to aiming to replicate Apple's own apps. In a variation of Gordon Ramsey's taste it then make it, the challenge was to re-create the iPhone's 'Notes' app.", site_url: null, github_url: 'https://github.com/jro31/Notes' }
+  ]
+}
+
+function addTimelineGaps() {
+  $('.timeline-gap').addClass('row').append(
+    '<div class="col-12 col-md-10 offset-md-1">' +
+      '<div class="d-flex justify-content-center">' +
+        '<div class="col-2 pl-2 pr-0 pl-md-5"></div>' +
+        '<div class="col-2 col-md-1 px-0 d-flex flex-column align-items-center">' +
+          '<div class="timeline-line"></div>' +
+        '</div>' +
+        '<div class="col-8 col-md-9 pl-0 pl-md-2 pr-2 pr-md-5"></div>' +
+      '</div>' +
+    '</div>'
+  )
+}
+
 function typeWriter() {
   var shortPause = 500
   var longPause = 3000
@@ -94,21 +152,11 @@ function typeWriter() {
 }
 
 $('body').on('click', '#hamburger', function(click) {
-  // console.log($('#mobile-menu-items').attr('class'))
   $('#mobile-menu-items').toggleClass('d-none')
 })
-
-// $(document).click(function(click) {
-//   if (click.target.id === 'hamburger') {
-//     $('#mobile-menu-items').toggleClass('d-none')
-//   }
-// })
-
-// $('#hamburger').click(function() {
-//   console.log('&&&&&&&&&&&&&&')
-// })
 
 window.onload = function() {
   populateNavbar();
   typeWriter();
+  populateTimeline();
 }
